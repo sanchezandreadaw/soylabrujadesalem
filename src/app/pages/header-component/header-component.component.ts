43628e383ef48edd1c466b3b8349ff84538909f9
sanchezandreadaw <sanchezmoraandreadaw@gmail.com>
2025-01-15 19:08:15 +0100

    Creacion de los primeros componentes que iran en el home e instalacion de bootstrap

---
+++ src/app/pages/header-component/header-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-component.component.html',
+  styles: ``
+})
+export class HeaderComponentComponent {
+
+}
---
+++ src/app/pages/learn-component/learn-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-learn-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './learn-component.component.html',
+  styles: ``
+})
+export class LearnComponentComponent {
+
+}
